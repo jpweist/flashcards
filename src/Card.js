ee6b906defb9f04e73b4265791c1457f4ed43af9
Jim Weist <jp_weist@yahoo.com>
2019-09-30 18:50:16 -0600

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+class Card {
+  constructor(num, question, answers, correctAnswer) {
+    this.num = 1;
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  }
+
+
+}
+
+module.exports = Card;
